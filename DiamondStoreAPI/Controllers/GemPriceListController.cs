64671424b46c6f7ab9a2d802c05b8fedddf7ec74
Nguyen Hoai Nam <namnhse161728@fpt.edu.vn>
2024-06-09 13:03:56 +0700

    Gem Price List

--- DiamondStoreAPI/Controllers/GemPriceListController.cs
+++ DiamondStoreAPI/Controllers/GemPriceListController.cs
@@ -15,95 +15,80 @@ namespace DiamondStoreAPI.Controllers
     [ApiController]
     public class GemPriceListController : ControllerBase
     {
-        //    private readonly IGemPriceListService iGemPriceListService;
+        private readonly IGemPriceListService iGemPriceListService;
 
-        //    public GemPriceListController(IGemPriceListService gemPriceListService)
-        //    {
-        //        iGemPriceListService = gemPriceListService;
-        //    }
-
-        //    // GET: api/GemPriceList
-        //    [HttpGet]
-        //    public async Task<ActionResult<IEnumerable<TblGemPriceList>>> GetTblGemPriceLists()
-        //    {
-        //        return await iGemPriceListService.TblGemPriceLists.ToListAsync();
-        //    }
-
-        //    // GET: api/GemPriceList/5
-        //    [HttpGet("{id}")]
-        //    public async Task<ActionResult<TblGemPriceList>> GetTblGemPriceList(int id)
-        //    {
-        //        var tblGemPriceList = await iGemPriceListService.TblGemPriceLists.FindAsync(id);
-
-        //        if (tblGemPriceList == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        return tblGemPriceList;
-        //    }
-
-        //    // PUT: api/GemPriceList/5
-        //    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //    [HttpPut("{id}")]
-        //    public async Task<IActionResult> PutTblGemPriceList(int id, TblGemPriceList tblGemPriceList)
-        //    {
-        //        if (id != tblGemPriceList.Id)
-        //        {
-        //            return BadRequest();
-        //        }
-
-        //        iGemPriceListService.Entry(tblGemPriceList).State = EntityState.Modified;
-
-        //        try
-        //        {
-        //            await iGemPriceListService.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!TblGemPriceListExists(id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-
-        //        return NoContent();
-        //    }
-
-        //    // POST: api/GemPriceList
-        //    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //    [HttpPost]
-        //    public async Task<ActionResult<TblGemPriceList>> PostTblGemPriceList(TblGemPriceList tblGemPriceList)
-        //    {
-        //        iGemPriceListService.TblGemPriceLists.Add(tblGemPriceList);
-        //        await iGemPriceListService.SaveChangesAsync();
-
-        //        return CreatedAtAction("GetTblGemPriceList", new { id = tblGemPriceList.Id }, tblGemPriceList);
-        //    }
-
-        //    // DELETE: api/GemPriceList/5
-        //    [HttpDelete("{id}")]
-        //    public async Task<IActionResult> DeleteTblGemPriceList(int id)
-        //    {
-        //        var tblGemPriceList = await iGemPriceListService.TblGemPriceLists.FindAsync(id);
-        //        if (tblGemPriceList == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        iGemPriceListService.TblGemPriceLists.Remove(tblGemPriceList);
-        //        await iGemPriceListService.SaveChangesAsync();
-
-        //        return NoContent();
-        //    }
-
-        //    private bool TblGemPriceListExists(int id)
-        //    {
-        //        return iGemPriceListService.TblGemPriceLists.Any(e => e.Id == id);
-        //    }
+        public GemPriceListController(IGemPriceListService gemPriceListService)
+        {
+            iGemPriceListService = gemPriceListService;
         }
+
+        // GET: api/GemPriceList
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblGemPriceList>>> GetTblGemPriceLists()
+        {
+            if (iGemPriceListService.GetGemPriceLists() == null) {
+                return NotFound();
+            }
+            return iGemPriceListService.GetGemPriceLists();
+        }
+
+        // GET: api/GemPriceList/5
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<TblGemPriceList>> GetTblGemPriceList(int id)
+        //{
+        //    var tblGemPriceList = await iGemPriceListService.TblGemPriceLists.FindAsync(id);
+
+        //    if (tblGemPriceList == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return tblGemPriceList;
+        //}
+
+        // PUT: api/GemPriceList/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTblGemPriceList(int id, TblGemPriceList tblGemPriceList)
+        {
+            if (id != tblGemPriceList.Id)
+            {
+                return BadRequest();
+            }
+
+            var isUpdate = iGemPriceListService.UpdateGemPriceList;
+
+            return NoContent();
+        }
+
+        // POST: api/GemPriceList
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TblGemPriceList>> PostTblGemPriceList(TblGemPriceList tblGemPriceList)
+        {
+            TblGemPriceList newGemPirce = iGemPriceListService.AddGemPriceList(tblGemPriceList);
+            return CreatedAtAction("GetTblGemPriceList", new { id = tblGemPriceList.Id }, tblGemPriceList);
+        }
+
+        // DELETE: api/GemPriceList/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteTblGemPriceList(int id)
+        //{
+        //    var tblGemPriceList = await iGemPriceListService.TblGemPriceLists.FindAsync(id);
+        //    if (tblGemPriceList == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    iGemPriceListService.TblGemPriceLists.Remove(tblGemPriceList);
+        //    await iGemPriceListService.SaveChangesAsync();
+
+        //    return NoContent();
+        //}
+
+        //private bool TblGemPriceListExists(int id)
+        //{
+        //    return iGemPriceListService.TblGemPriceLists.Any(e => e.Id == id);
+        //}
     }
+}
