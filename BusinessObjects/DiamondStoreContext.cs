671bb30cf2c6b22bac85d3a11c46f902160c8b93
LeonOthT <trieuothse173617@fpt.edu.vn>
2024-06-13 17:00:53 +0700

    edit connection string

--- BusinessObjects/DiamondStoreContext.cs
+++ BusinessObjects/DiamondStoreContext.cs
@@ -53,7 +53,7 @@ public partial class DiamondStoreContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;uid=sa;pwd=12345;database=DiamondStore;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-BR0QKCC\\SQL2019;Initial Catalog=DiamondStore;Persist Security Info=True;User ID=sa;Password=12345;Encrypt=True;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
