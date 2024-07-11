1154f749e8b242677b11bbf6dc99ea0726f3f23e
Van Hai <haivv.se@gmail.com>
2024-07-11 15:43:19 +0700

    Add html page

--- DiamondStoreAPI/Program.cs
+++ DiamondStoreAPI/Program.cs
@@ -395,6 +395,9 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "DiamondStoreAPI v1");
 });
 
+app.UseDefaultFiles(); // Để sử dụng index.html làm tệp mặc định
+app.UseStaticFiles(); // Để phục vụ các tệp tĩnh trong wwwroot
+
 app.MapControllers();
 
 app.Run();
