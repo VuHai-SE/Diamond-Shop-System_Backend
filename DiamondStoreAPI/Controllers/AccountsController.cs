463932b70c2806ec19f2f30f87de2c4a423f7438
Van Hai <haivv.se@gmail.com>
2024-07-22 09:29:18 +0700

    deploy

--- DiamondStoreAPI/Controllers/AccountsController.cs
+++ DiamondStoreAPI/Controllers/AccountsController.cs
@@ -231,7 +231,7 @@ namespace DiamondStoreAPI.Controllers
                     new Claim(ClaimTypes.Name, account.Username),
                     new Claim(ClaimTypes.Role, account.Role)
                 }),
-                Expires = DateTime.UtcNow.AddDays(7),
+                Expires = DateTime.UtcNow.AddDays(1),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
